71c4bf5de1c3338a184a71697a1e6592ef806815
Julian Cruz <freakcruz@me.com>
2015-04-07 04:14:18 -0500

    Se agrega codigo en app para logout de usuarios

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask.ext.bcrypt import check_password_hash
-from flask.ext.login import LoginManager, login_user
+from flask.ext.login import (LoginManager, login_user, logout_user, login_required)
 
 import forms
 import models
@@ -69,6 +69,16 @@ def login():
       else:
         flash("You email or password doesn't match¡", "error")
         return render_template('login.html', form=form)
+
+
+
+@app.route('/logout')
+@login_required
+    def logout():
+      logout_user()
+          flash("You've been logged out¡ Come back soon!", "success")
+        return redirect(url_for('index'))
+
       
 
 
